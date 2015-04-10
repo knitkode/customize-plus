f90a96d81cd8d5904eff6fa74f250fd27b16b8ee
kuus <kunderikuus@gmail.com>
2015-04-10 04:46:43 +0200

    renamed to pluswp

---
+++ config/gulp/common-plugins.js
@@ -0,0 +1,40 @@
+/* jshint node: true */
+
+/**
+ * Gulp Plugins shared options
+ * @type {Object}
+ */
+module.exports = {
+  // gulp-modernizr shared options
+  modernizr: {
+    'cache' : true,
+    'options': [ // Based on default settings on http://modernizr.com/download/
+      'setClasses',
+      'addTest',
+      'testProp',
+      'fnBind'
+    ],
+    'excludeTests': [],
+    'crawl' : false,
+    'customTests' : []
+  },
+  uglify: {
+    preserveComments: 'some',
+    toplevel: true,
+    mangle: true,
+    compress: {
+      drop_console: true,
+      global_defs: {
+        DEBUG: false
+      }
+    }
+  },
+  concat: {
+    newLine: '\n\n'
+  },
+  autoprefixer: 'last 2 version',
+  jsonEditor: {
+    'indent_char': ' ',
+    'indent_size': 2
+  }
+};
