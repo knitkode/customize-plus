04fe45097798de6183fe59b757b726d75b932cfb
kuus <kunderikuus@gmail.com>
2015-11-02 12:10:43 +0000

    skeleton small change

--- src/views/customize-loader.php
+++ src/views/customize-loader.php
@@ -1,13 +1,13 @@
 <div id="pwpcp-loader-preview" class="wp-full-overlay-main pwpcp-overlay--preview">
 	<div class="pwpcpui-midpoint-wrap">
 		<div class="pwpcpui-midpoint">
-			<img src="<?php echo esc_url( plugins_url( 'assets/images/logo-white.png', PWPCP_PLUGIN_FILE ) ); ?>">
+			<img id="pwpcp-loader-img" src="<?php echo esc_url( plugins_url( 'assets/images/logo-white.png', PWPCP_PLUGIN_FILE ) ); ?>">
 			<?php if ( isset ( $_GET['pwpcp_import'] ) ): // input var okay ?>
-				<h1 id="pwpcp-title" class="pwpcp-text"><?php _e( 'Import done' ); ?></h1>
-				<h3 id="pwpcp-text" class="pwpcp-text"><?php _e( 'All options have been succesfully imported and saved' ); ?></h3>
+				<h1 id="pwpcp-loader-title" class="pwpcp-text"><?php _e( 'Import done' ); ?></h1>
+				<h3 id="pwpcp-loader-text" class="pwpcp-text"><?php _e( 'All options have been succesfully imported and saved' ); ?></h3>
 			<?php else : ?>
-				<h1 id="pwpcp-title" class="pwpcp-text"><?php echo 'Customize Plus'; ?></h1>
-				<h3 id="pwpcp-text" class="pwpcp-text"></h3>
+				<h1 id="pwpcp-loader-title" class="pwpcp-text"><?php echo 'Customize Plus'; ?></h1>
+				<h3 id="pwpcp-loader-text" class="pwpcp-text"></h3>
 			<?php endif; ?>
 			<div class="pwpcp-text">
 				<span class="spinner"></span>
