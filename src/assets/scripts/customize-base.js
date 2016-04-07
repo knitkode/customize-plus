b92111576f282c19c3bcfe9b27fe1f97873559bf
kuus <kunderikuus@gmail.com>
2016-04-07 13:30:33 +0100

    better indentation for js

--- src/assets/scripts/customize-base.js
+++ src/assets/scripts/customize-base.js
@@ -1,27 +1,27 @@
 //=include customize/tpl-begin.js
 
-//=include customize/core/setup-globals.js
+  //=include customize/core/setup-globals.js
 
-//=require customize/core/setup-jquery.js
-//=require customize/core/setup-markdown.js
+  //=require customize/core/setup-jquery.js
+  //=require customize/core/setup-markdown.js
 
-//=require customize/core/regexes.js
-//=require customize/core/utils.js
-//=require customize/core/validators.js
-//=require customize/core/wptight.js
-//=require customize/core/banner.js
-//=require customize/core/skeleton.js
-//=require customize/core/tabs.js
+  //=require customize/core/regexes.js
+  //=require customize/core/utils.js
+  //=require customize/core/validators.js
+  //=require customize/core/wptight.js
+  //=require customize/core/banner.js
+  //=require customize/core/skeleton.js
+  //=require customize/core/tabs.js
 
-//=require customize/controls/base.js
+  //=require customize/controls/base.js
 
-$document.ready(function() {
-  if (DEBUG.performances) var t = performance.now();
-  WpTight.init();
-  Tabs.init();
-  if (DEBUG.performances) console.log( 'Customize.js Base (ready start->end) took ' + (performance.now() - t) + ' ms.');
-});
+  $document.ready(function() {
+    if (DEBUG.performances) var t = performance.now();
+    WpTight.init();
+    Tabs.init();
+    if (DEBUG.performances) console.log( 'Customize.js Base (ready start->end) took ' + (performance.now() - t) + ' ms.');
+  });
 
-//=require customize/temp.js
+  //=require customize/temp.js
 
 //=include customize/tpl-end.js
--- src/assets/scripts/customize.js
+++ src/assets/scripts/customize.js
@@ -1,25 +1,25 @@
 //=include customize/tpl-begin.js
 
-//=include customize/core/setup-globals.js
+  //=include customize/core/setup-globals.js
 
-//=require customize/controls/base-input.js
-//=require customize/controls/base-radio.js
+  //=require customize/controls/base-input.js
+  //=require customize/controls/base-radio.js
 
-//=require customize/controls/buttonset.js
-//=require customize/controls/color.js
-//=require customize/controls/dummy.js
-//=require customize/controls/font-family.js
-//=require customize/controls/multicheck.js
-//=require customize/controls/number.js
-//=require customize/controls/radio.js
-//=require customize/controls/radio-image.js
-//=require customize/controls/select.js
-//=require customize/controls/font-weight.js
-//=require customize/controls/slider.js
-//=require customize/controls/sortable.js
-//=require customize/controls/tags.js
-//=require customize/controls/text.js
-//=require customize/controls/textarea.js
-//=require customize/controls/toggle.js
+  //=require customize/controls/buttonset.js
+  //=require customize/controls/color.js
+  //=require customize/controls/dummy.js
+  //=require customize/controls/font-family.js
+  //=require customize/controls/multicheck.js
+  //=require customize/controls/number.js
+  //=require customize/controls/radio.js
+  //=require customize/controls/radio-image.js
+  //=require customize/controls/select.js
+  //=require customize/controls/font-weight.js
+  //=require customize/controls/slider.js
+  //=require customize/controls/sortable.js
+  //=require customize/controls/tags.js
+  //=require customize/controls/text.js
+  //=require customize/controls/textarea.js
+  //=require customize/controls/toggle.js
 
 //=include customize/tpl-end.js
