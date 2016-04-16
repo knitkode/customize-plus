043c1fdf634bed0c3b0a33f6f40d2fe21ea5ce2e
kuus <kunderikuus@gmail.com>
2016-04-16 16:13:57 +0100

    dynamic width labels on toggle

--- src/includes/controls/toggle.php
+++ src/includes/controls/toggle.php
@@ -29,13 +29,14 @@ class PWPcp_Customize_Control_Toggle extends PWPcp_Customize_Control_Checkbox {
 	 */
 	protected function js_tpl() {
 		?>
+		<# var label0 = data.attrs.label_0; label1 = data.attrs.label_1; #>
 		<# if (data.label) { #><div class="customize-control-title">{{{ data.label }}}</div><# } #>
 		<# if (data.description) { #><div class="description customize-control-description">{{{ data.description }}}</div><# } #>
-		<label class="switch-light pwpcpui-switch<# if (data.attrs.label_0 || data.attrs.label_1) { #> pwpcpui-switch__labelled<# } #>" onclick="">
+		<label class="switch-light pwpcpui-switch<# if (label0 && label1) { var l0l = label0.length, l1l = label1.length; #><# if ((l0l && l1l) && (Math.abs(l0l - l1l) > 1) || l0l > 6 || l1l > 6) { #> pwpcpui-switch__labelsauto<# } else { #> pwpcpui-switch__labels<# } } #>" onclick="">
 		  <input type="checkbox" name="_customize-pwpcp_toggle-{{ data.id }}" value="<?php // filled through js ?>" <# var a = data.attrs; for (var key in a) { if (a.hasOwnProperty(key)) { #>{{ key }}="{{ a[key] }}" <# } } #> <# if (data.value) { #>checked<# } #>>
 		  <span>
-		    <span><# if (data.attrs.label_0) { #>{{{data.attrs.label_0}}}<# } #></span>
-		    <span><# if (data.attrs.label_1) { #>{{{data.attrs.label_1}}}<# } #></span>
+		    <span><# if (label0) { #>{{{data.attrs.label_0}}}<# } #></span>
+		    <span><# if (label1) { #>{{{data.attrs.label_1}}}<# } #></span>
 		    <a></a>
 		  </span>
 		</label>
