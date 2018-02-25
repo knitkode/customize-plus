99c6a75ab9f78cf482a28be587266ec1c3db34c0
kuus <kunderikuus@gmail.com>
2018-02-25 12:23:10 +0100

    workflow seems ok

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -10,4 +10,4 @@ const customize = require('./gulp/customize');
 gulp.task('build', gulp.parallel(base.build, customize.build, customize.docs));
 gulp.task('watch', gulp.series('build', gulp.parallel(base.watch, customize.watch)));
 gulp.task('deploy', gulp.series('build', wpRelease, wpDeploy));
-gulp.task('publish', gulp.series(/*wpPublishSvn,*/ wpTagVersion));
+gulp.task('publish', gulp.series(wpPublishSvn, wpTagVersion));
