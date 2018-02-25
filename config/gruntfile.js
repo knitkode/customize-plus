d586fcca5c1854e0619a50c43cd2b62c65a5db33
kuus <kunderikuus@gmail.com>
2018-02-25 12:18:50 +0100

    workflow

--- config/gruntfile.js
+++ config/gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
 
   grunt.file.setBase('../');
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     wp_deploy: {
@@ -12,17 +11,14 @@ module.exports = function(grunt) {
         options: {
           plugin_slug: '<%= pkg.config.slug %>',
           svn_user: 'knitkode',
-          build_dir: paths.DIST, // relative path to your build directory
-          assets_dir: paths.dist.assets // relative path to your assets directory (optional).
+          build_dir: paths.DIST,
+          assets_dir: paths.dist.assets
         },
       }
     },
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-wp-deploy');
 
-  // Default task(s).
   grunt.registerTask('publish', ['wp_deploy']);
-
 };
--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const wpRelease = require('./dev-lib/gulp/wpRelease');
 const wpDeploy = require('./dev-lib/gulp/wpDeploy');
+const wpPublishSvn = require('./dev-lib/gulp/wpPublishSvn');
+const wpTagVersion = require('./dev-lib/gulp/wpTagVersion');
 const base = require('./gulp/base');
 const customize = require('./gulp/customize');
 
@@ -8,3 +10,4 @@ const customize = require('./gulp/customize');
 gulp.task('build', gulp.parallel(base.build, customize.build, customize.docs));
 gulp.task('watch', gulp.series('build', gulp.parallel(base.watch, customize.watch)));
 gulp.task('deploy', gulp.series('build', wpRelease, wpDeploy));
+gulp.task('publish', gulp.series(wpPublishSvn, wpTagVersion));
