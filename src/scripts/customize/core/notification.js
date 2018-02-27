d279baf6a95b3832f62dabef8f5c7785fec78d38
kuus <kunderikuus@gmail.com>
2018-02-27 22:42:31 +0100

    tabs to spaces

--- src/scripts/customize/core/notification.js
+++ src/scripts/customize/core/notification.js
@@ -15,66 +15,62 @@ import Utils from '../core/utils';
  */
 class Notification extends wpApi.Notification {
 
-	// constructor (code, params) {
-	// 	super.initialize(code, params);
-	// }
+  /**
+   * @since 1.1.0
+   *
+   * @override
+   * @return {jQuery} Notification container element.
+   */
+  render () {
+    var notification = this, container, data;
+    if ( ! notification.template ) {
+      // @note tweak is done here, template string instead of an id
+      notification.template = Utils.template( this._tpl() );
+    }
+    data = _.extend( {}, notification, {
+      alt: notification.parent && notification.parent.alt
+    } );
+    container = $( notification.template( data ) );
 
-	/**
-	 * @since 1.1.0
-	 *
-	 * @override
-	 * @return {jQuery} Notification container element.
-	 */
-	render () {
-		var notification = this, container, data;
-		if ( ! notification.template ) {
-			// @note tweak is done here, template string instead of an id
-			notification.template = Utils.template( this._tpl() );
-		}
-		data = _.extend( {}, notification, {
-			alt: notification.parent && notification.parent.alt
-		} );
-		container = $( notification.template( data ) );
+    if ( notification.dismissible ) {
+      container.find( '.notice-dismiss' ).on( 'click keydown', function( event ) {
+        if ( 'keydown' === event.type && 13 !== event.which ) {
+          return;
+        }
 
-		if ( notification.dismissible ) {
-			container.find( '.notice-dismiss' ).on( 'click keydown', function( event ) {
-				if ( 'keydown' === event.type && 13 !== event.which ) {
-					return;
-				}
+        if ( notification.parent ) {
+          notification.parent.remove( notification.code );
+        } else {
+          container.remove();
+        }
+      });
+    }
 
-				if ( notification.parent ) {
-					notification.parent.remove( notification.code );
-				} else {
-					container.remove();
-				}
-			});
-		}
+    return container;
+  }
 
-		return container;
-	}
-
-	/**
-	 * Template
-	 *
-	 * For now it's the same as the WordPress default one plus markdown support
-	 *
-	 * @since 1.1.0
-	 *
+  /**
+   * Template
+   *
+   * For now it's the same as the WordPress default one plus markdown support
+   *
+   * @since 1.1.0
+   *
    * @memberof! controls.Base#
    * @access package
    *
-	 * @return {string}
-	 */
-	_tpl () {
-		return 	`
-			<li class="notice notice-{{ data.type || 'info' }} {{ data.alt ? 'notice-alt' : '' }} {{ data.dismissible ? 'is-dismissible' : '' }} {{ data.containerClasses || '' }} kkcp-notification" data-code="{{ data.code }}" data-type="{{ data.type }}">
-				<# if (marked) { #>{{{ marked(data.message || data.code) }}}<# } else { #><div class="notification-message">{{{ data.message || data.code }}}</div><# } #>
-				<# if ( data.dismissible ) { #>
-					<button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php esc_html( 'Dismiss' ) ?></span></button>
-				<# } #>
-			</li>
-		`
-	}
+   * @return {string}
+   */
+  _tpl () {
+    return  `
+      <li class="notice notice-{{ data.type || 'info' }} {{ data.alt ? 'notice-alt' : '' }} {{ data.dismissible ? 'is-dismissible' : '' }} {{ data.containerClasses || '' }} kkcp-notification" data-code="{{ data.code }}" data-type="{{ data.type }}">
+        <# if (marked) { #>{{{ marked(data.message || data.code) }}}<# } else { #><div class="notification-message">{{{ data.message || data.code }}}</div><# } #>
+        <# if ( data.dismissible ) { #>
+          <button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php esc_html( 'Dismiss' ) ?></span></button>
+        <# } #>
+      </li>
+    `
+  }
 }
 
 export default api.core.Notification = Notification;
